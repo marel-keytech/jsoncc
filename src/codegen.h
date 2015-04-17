b508853e1ae532af44986df8a8edf931404abce8
Andri Yngvason <any1@hi.is>
2015-04-17 20:50:57 +0000

    cleaning up old junk

--- src/codegen.h
+++
@@ -1,7 +0,0 @@
-#ifndef CODECEN_H_DEFINED
-#define CODECEN_H_DEFINED
-
-void codegen_header(const char* name, const struct obj* obj);
-void codegen_source(const char* name, const struct obj* obj);
-
-#endif /* CODECEN_H_DEFINED */
